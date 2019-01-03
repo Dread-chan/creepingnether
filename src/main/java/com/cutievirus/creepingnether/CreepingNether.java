1a26c4d6947ab72039061902159abd3e060cef66
Dread-chan <dreadwing93@ymail.com>
2019-01-02 20:21:05 -0500

    2.2.2

--- src/main/java/com/cutievirus/creepingnether/CreepingNether.java
+++ src/main/java/com/cutievirus/creepingnether/CreepingNether.java
@@ -32,7 +32,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class CreepingNether {
 
 	public static final String MODID = "creepingnether";
-	public static final String VERSION = "2.2.1";
+	public static final String VERSION = "2.2.2";
 
 	@SidedProxy(clientSide="com.cutievirus.creepingnether.ClientProxy", serverSide="com.cutievirus.creepingnether.CommonProxy")
 	public static CommonProxy proxy;
